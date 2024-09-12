560f0c11704fe20ff0a3dbc38375c0a7a6fc1673
iinsue <hinsu93@naver.com>
2024-09-12 21:32:28 +0900

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,5 +1,11 @@
 import NextAuth from "next-auth";
+import Kakao from "next-auth/providers/kakao";
 
-export const {handlers, signIn, signOut, auth} = NextAuth({
-    providers:[],
-})
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Kakao({
+      clientId: process.env.AUTH_KAKAO_ID,
+      clientSecret: process.env.AUTH_KAKAO_SECRET,
+    }),
+  ],
+});
