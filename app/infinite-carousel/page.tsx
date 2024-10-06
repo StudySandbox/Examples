38b41b2b80e6e89bf93074526278ad8ae2e4727d
iinsue <hinsu93@gmail.com>
2024-10-06 21:12:27 +0900

    Update page.tsx

--- app/infinite-carousel/page.tsx
+++ app/infinite-carousel/page.tsx
@@ -2,15 +2,11 @@ import { CarouselComponent } from "./components";
 
 const InfiniteCarouselPage = () => {
   return (
-    <div className="flex min-h-screen w-full items-center">
-      <div className="h-[800px] w-full overflow-hidden">
-        <div className="flex">
-          <CarouselComponent direction="up" />
-          <div className="flex flex-1 items-center justify-center bg-blue-200">
-            Main
-          </div>
-          <CarouselComponent direction="down" />
-        </div>
+    <div className="relative max-h-screen w-full overflow-hidden bg-black">
+      <div className="flex min-h-screen w-full items-center justify-center space-y-4">
+        <CarouselComponent direction="up" />
+        <div className="h-full w-full" />
+        <CarouselComponent direction="down" />
       </div>
     </div>
   );
