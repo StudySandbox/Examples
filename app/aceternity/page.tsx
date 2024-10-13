7ad235c796d4b232ba11a1c1e8880325f5a1ab8f
iinsue <hinsu93@gmail.com>
2024-10-13 22:54:25 +0900

    add background-gradient

---
+++ app/aceternity/page.tsx
@@ -0,0 +1,11 @@
+import { BackgroundGradientComponent } from "./components/background-gradient";
+
+const AceternityPage = () => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <BackgroundGradientComponent />
+    </div>
+  );
+};
+
+export default AceternityPage;
